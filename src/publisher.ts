7ca5897a8599b550aaf00b446592897591805f2d
Kyohei Ito <je.suis.kyohei+github@gmail.com>
2026-01-14 13:07:56 +0900

    feat: add deployment details logging

--- src/publisher.ts
+++ src/publisher.ts
@@ -62,6 +62,9 @@ export class GhPagesPublisherPlugin implements PublisherPlugin<PluginConfig> {
           targetDir,
           commitMessage: this.commitMessage ?? `deploy: ${key}`,
         })
+        this.logger.info(
+          `Deployed to GitHub Pages: branch=${this.branch}, targetDir=${targetDir}`,
+        )
       } else {
         this.logger.warn(
           'Deployment skipped. Set outDir option or enable includeCommitHash.',
